03767eea1328aed2a10dabc8342fe2d29c065658
sikadm <33527388+sikadm@users.noreply.github.com>
2017-12-05 17:05:29 -0500

    Update armed.vhd

--- armed.vhd
+++ armed.vhd
@@ -22,7 +22,17 @@ architecture arch of armed is
 	signal font_bit: std_logic;
 	signal armed_on: std_logic;
 	signal disarmed_on: std_logic;
+	signal counterA: std_logic_vector(14 downto 0);
+	signal counterB: std_logic_vector(12 downto 0);
 
+	component font_rom
+	   port(
+      clk: in std_logic;
+      addr: in std_logic_vector(10 downto 0);
+      data: out std_logic_vector(7 downto 0)
+   );
+	end component;
+	
 begin
 	pix_x <= unsigned(pixel_x);
 	pix_y <= unsigned(pixel_y);
@@ -31,13 +41,7 @@ begin
 	font1: font_rom port map(clk, rom_addr, font_word);
 
 --Display System Armed
-if textdisplay='1' then 
-armed_on <=
-	'1' when pix_y(9 downto 6)=3 and 5<= pix_x(9 downto 5) and pix_x(9 downto 5)<=13 else'0';
-	row_addr_a <= std_logic_vector(pix_y(5 downto 2));
-	bit_addr_a <= std_logic_vector(pix_x(4 downto 2));
-	with pix_x(8 downto 5) select
-	char_addr_a <=
+	with pix_x(8 downto 5) select char_addr_a <=
 		"1010011" when "0100", -- S
 		"1111001" when "0101", -- y
 		"1110011" when "0110", -- s
@@ -50,15 +54,11 @@ armed_on <=
 		"1101101" when "1101", -- m
 		"1100101" when "1110", -- e
 		"1100100" when "1111", -- d
+		"XXXXXXX" when others; --
+
 
 --Display System Disarmed
-else
-disarmed_on<=
-	'1' when pix_y(9 downto 6)=3 and 5<= pix_x(9 downto 5) and pix_x(9 downto 5)<=13 else'0';
-	row_addr_d <= std_logic_vector(pix_y(5 downto 2));
-	bit_addr_d <= std_logic_vector(pix_x(4 downto 2));
-	with pix_x(8 downto 5) select
-	char_addr_d <=
+	with pix_x(8 downto 5) select char_addr_d <=
 		"1010011" when "0001", -- S
 		"1111001" when "0010", -- y
 		"1110011" when "0011", -- s
@@ -74,8 +74,23 @@ disarmed_on<=
 		"1101101" when "1101", -- m
 		"1100101" when "1110", -- e
 		"1100100" when "1111", -- d
-end if;
+		"0000000" when others; --
+		
+process(pix_x, pix_y, textdisplay)
+begin
+if textdisplay='1' then 
+	armed_on <='1';
+	disarmed_on<='0';	
+	else
+	armed_on<='0';
+	disarmed_on<='1';
+	row_addr_a <= std_logic_vector(pix_y(5 downto 2));
+	bit_addr_a <= std_logic_vector(pix_x(4 downto 2));
+	row_addr_d <= std_logic_vector(pix_y(5 downto 2));
+	bit_addr_d <= std_logic_vector(pix_x(4 downto 2));
 
+end if;
+end process;
 -- mux for font ROM addresses and rgb
 process(pix_x, pix_y, font_bit,char_addr_a,row_addr_a,bit_addr_a, char_addr_d, row_addr_d, bit_addr_d)
 begin
@@ -86,11 +101,13 @@ begin
 		bit_addr <= bit_addr_a;
 		text_rgb <= "010";
 
-	elsif disarmed_on='1' then
+
+	else
 		char_addr <= char_addr_d;
 		row_addr <= row_addr_d;
 		bit_addr <= bit_addr_d;
 		text_rgb <= "100";
+
 end if;
 end process;
 	--if --switch flipped
@@ -101,7 +118,7 @@ end process;
 	
 
 -- font rom interface
-	rom_addr <= char_addr and row_addr;
+	rom_addr <= (char_addr & row_addr);
 	font_bit <= font_word(to_integer(unsigned(not bit_addr)));
 	
 end arch;
