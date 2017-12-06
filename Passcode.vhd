c8f9206d8c53fbd53d3e530a4ada03c8314a1c0e
kheiryrj <34217464+kheiryrj@users.noreply.github.com>
2017-12-06 16:46:37 -0500

    Update Passcode.vhd

--- Passcode.vhd
+++ Passcode.vhd
@@ -1,58 +1,90 @@
 Library IEEE;
 use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
 
-entity passcode is 
+entity TestSecuritySystem is 
 	port (clk : in std_logic;
 			but_n1, but_n2, but_n3 : in std_logic;
-			code : out std_logic);
-end passcode;
+			code, two : out std_logic);
+end TestSecuritySystem;
 	
-architecture beh of passcode is 
+architecture beh of TestSecuritySystem is 
 
 	type state_type is (STATE_A, STATE_B, STATE_C, STATE_D, STATE_E, STATE_F); 
 	signal state : state_type := STATE_A;
+	signal counter : std_logic_vector(31 downto 0);
 	
 begin
 	
 	process(clk, but_n1, but_n2, but_n3) 
 	begin
+	
 	if(rising_edge(clk)) then
+
 		case state is
 		
 		when STATE_A =>
-		if (but_n2'last_value = '1' and but_n2 = '0') then
+		two <= '0';
+		if (but_n2 = '0') then 	-- button2 has been pressed
+			two <= '1';
 			state <= STATE_B;
-		else
+		elsif (but_n1 = '0' or but_n3 = '0') then
 			state <= STATE_F;
+		else
+			if counter < "10110010110100000101111000000000" then
+				counter <= counter + 1;
+			else 
+				state <= STATE_F;
+			end if;
 		end if;
 		
 		when STATE_B =>
-		if (but_n1'last_value = '1' and but_n1 = '0') then
+		if but_n1 = '0' then
 			state <= STATE_C;
-		else 
+		elsif (but_n2 = '0' or but_n3 = '0') then
 			state <= STATE_F;
+		else 
+			if counter < "10110010110100000101111000000000" then
+				counter <= counter+ 1;
+			else 
+				state <= STATE_F;
+			end if;
 		end if;
 		
 		when STATE_C =>
-		if (but_n3'last_value = '1' and but_n3 = '0') then
+		if but_n3 = '0' then
 			state <= STATE_D;
-		else 
+		elsif (but_n1 = '0' or but_n2 = '0') then
 			state <= STATE_F;
+		else 
+			if counter < "10110010110100000101111000000000" then
+				counter <= counter+ 1;
+			else 
+				state <= STATE_F;
+			end if;
 		end if;
 		
 		when STATE_D =>
-		if (but_n1'last_value = '1' and but_n1 = '0') then
+		if but_n1 = '0' then
 			state <= STATE_E;
+		elsif (but_n2 = '0' or but_n3 = '0') then
+			state <= STATE_F;
 		else
-			state <= STATE_E;
+			if counter < "10110010110100000101111000000000" then
+				counter <= counter + 1;
+			else 
+				state <= STATE_F;
+			end if;
 		end if;
 		
-		when STATE_E => 
+		when STATE_E => 	-- code is correct
 			code <= '1';
 			
-		when STATE_F =>
+		when STATE_F => 	-- code is incorrect
 			code <= '0';
 		end case;
 		end if;
 	end process;
+	
 end beh;
