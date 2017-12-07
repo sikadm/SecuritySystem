9b54c934c52e8da6257bb91b8f5fb9b82d198e84
kheiryrj <34217464+kheiryrj@users.noreply.github.com>
2017-12-06 20:32:41 -0500

    Update sevenSegDisplay.vhd

--- sevenSegDisplay.vhd
+++ sevenSegDisplay.vhd
@@ -1,12 +1,12 @@
 Library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
-entity TestSecuritySystem is 
+entity sevenSegDisplay is 
 	port (in0, in1, in2, in3 : in std_logic;
 			a, b, c, d, e, f, g : out std_logic);
-end TestSecuritySystem;
+end sevenSegDisplay;
 	
-architecture beh of TestSecuritySystem is 
+architecture beh of sevenSegDisplay is 
 begin
 	process(in0, in1, in2, in3) begin
 			a <= NOT ((in0 AND NOT in1 AND NOT in2) OR (NOT in0 AND in2) OR (in1 AND in2) OR (in0 AND NOT in3) OR (NOT in1 AND NOT in3) OR (NOT in0 AND in1 AND in3));
