49ed41a70c3db649bf9c0b3fbf96049233d1197d
kheiryrj <34217464+kheiryrj@users.noreply.github.com>
2017-12-06 23:49:50 -0500

    Update SecuritySystem.vhd

--- SecuritySystem.vhd
+++ SecuritySystem.vhd
@@ -3,50 +3,51 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.STD_LOGIC_ARITH.all;
 use IEEE.STD_LOGIC_UNSIGNED.all;
 
-entity SecSys is 
+entity Final_Security_System is 
 	port (reset, clk : in std_logic;
 			but_n1, but_n2, but_n3 : in std_logic;
 			a1, b1, c1, d1, e1, f1, g1 : out std_logic;
 			a2, b2, c2, d2, e2, f2, g2 : out std_logic;
 			LED1, LED2, LED3, LED4, LED5, LED6 : out std_logic;
 			door_n, armed : in std_logic);
-	end SecSys;
+	end Final_Security_System;
 	
-architecture beh of SecSys is 
+architecture beh of Final_Security_System is 
 
 type state_type is (STATE_Armed, STATE_Count, STATE_Alarm, STATE_Disarmed); 
 signal state : state_type := STATE_Disarmed;
-signal tens, ones : std_logic_vector(3 downto 0);
+signal tens, ones : std_logic_vector(3 downto 0);	-- tens and ones are BCD that will map to countdown display's inputs
 signal counter10 : std_logic_vector(28 downto 0);
-signal counter1 : std_logic_vector(25 downto 0);
-signal in0, in1, in2, in3 : std_logic;
+signal counter1, counter : std_logic_vector(25 downto 0);
+signal in0, in1, in2, in3 : std_logic; 				-- inputs for countdown display
 signal code : std_logic_vector(1 downto 0);
 signal lights : std_logic;
 
-	component sevenSegDisplay 
+	component sevenSegDisplay 			-- declare countdown display component
 		port (in0, in1, in2, in3 : in std_logic;
 			a, b, c, d, e, f, g : out std_logic); 
 	end component;
 					 
-	component passcode
-		port (clk : in std_logic;
+	component passcode				-- declare component for detecting correct passcode
+		port (clk, reset : in std_logic;
 				but_n1, but_n2, but_n3 : in std_logic;
 				code : out std_logic_vector(1 downto 0));
 	end component;
 	
 begin
 	
-	display10s : sevenSegDisplay port map
+	display10s : sevenSegDisplay port map				-- instantiate display component for tens place
 					(tens(3), tens(2), tens(1), tens(0),
 					 a1, b1, c1, d1, e1, f1, g1);
 					 
-	display1s : sevenSegDisplay port map
+	display1s : sevenSegDisplay port map				-- instantiate display component for ones place
 					(ones(3), ones(2), ones(1), ones(0),
 					 a2, b2, c2, d2, e2, f2, g2);
 					 
+	passcode1 : passcode port map 						-- instantiate passcode detector component
+					(clk, reset, but_n1, but_n2, but_n3, code);
 	
-	
-	LED1 <= lights;
+	LED1 <= lights;		-- all LEDs for the alarm flash in unison
 	LED2 <= lights;
 	LED3 <= lights;
 	LED4 <= lights;
@@ -57,65 +58,66 @@ begin
 	begin
 		if reset = '1' then              
 			state <= STATE_Disarmed;
+			
 		elsif rising_edge(clk) then
 			case state is
 			
-				when STATE_Disarmed =>
+				when STATE_Disarmed => 			-- When disarmed, most signals are set to 0 so they can restart when armed
 					tens <= "0000";
 					ones <= "0000";
+					counter10 <= (others => '0');
+					counter1 <= (others => '0');
+					counter <= (others => '0');
 					lights <= '0';
-					code <= "01";
 					if armed = '1' then
 						state <= STATE_Armed;
 					else 
-						state <= STATE_Disarmed;
+						state <= STATE_Disarmed; 		-- stay disarmed until "armed" switch is flipped
 					end if;
 			
-				when STATE_Armed =>
-					if door_n = '0' then
-						tens <= "0110";
-						ones <= "0000";
-						code <= "01";
+				when STATE_Armed => 			
+					if door_n = '0' then		-- stay armed until door is opened
+						tens <= "0101";		-- set tens to 5 and ones to 9 (start at 59)
+						ones <= "1001";
 						state <= STATE_Count;
 					else
 						state <= STATE_Armed;
 					end if;
 				
 				when STATE_Count =>
-					
-						if counter10 < "11101110011010110010100000000" then
-							counter10 <= counter10 + 1;
-						else 
-							tens <= tens - 1;
-							counter10 <= (others => '0');
-						end if;
-					
-						if counter1 < "10111110101111000010000000" then
-							counter1 <= counter1 + 1;
-						elsif ones = "0000" then
-							ones <= "1001";
-							counter1 <= (others => '0');
-						else 
-							ones <= ones - 1;
-							counter1 <= (others => '0');
-						end if;
-					
-					if code = "11" then
-						state <= STATE_Disarmed;
-					elsif ((tens = "0000" and ones = "0000") or code = "00") then
-						state <= STATE_Alarm;
+						
+					if counter1 < "10111110101111000010000000" then 	-- counts 1 second
+						counter1 <= counter1 + 1;
+					elsif ones = "0000" then				-- loops back to 9 seconds after 0
+						ones <= "1001";
+						counter1 <= (others => '0');
+					else 
+						ones <= ones - 1;						-- otherwise, decrements
+						counter1 <= (others => '0');
+					end if;
+						
+					if counter10 < "11101110011010110010100000000" then 	-- counts up to 10 seconds
+						counter10 <= counter10 + 1;
+					else 
+						tens <= tens - 1;												-- decrements every 10 seconds
+						counter10 <= (others => '0');
 					end if;
 					
+					if code = "11" then				-- passcode was entered correctly
+						state <= STATE_Disarmed;
+					elsif ((tens = "0000" and ones = "0000") or code = "00") then	-- countdown ends or code was entered incorrectly
+						state <= STATE_Alarm;
+					end if;
 				
 				when STATE_Alarm =>
-						if counter1 < "1011111010111100001000000" then
-							counter1 <= counter1 + 1;
+						if counter < "1011111010111100001000000" then 	-- counter to make LEDs blink every second
+							counter <= counter + 1;
 						else
 							lights <= not lights;
-							counter1 <= (others => '0');
+							counter <= (others => '0');
 						end if;
 
-					state <= STATE_Alarm;
+					state <= STATE_Alarm; 	-- stays in alarm state unless reset
 					
 			end case;
 		end if;
